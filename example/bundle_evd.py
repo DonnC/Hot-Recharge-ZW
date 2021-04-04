e659ed45021b98e026044f9f06e34b1537fe277e
Donald Chinhuru <donnclab@gmail.com>
2021-04-04 09:11:09 +0200

    Update bundle_evd.py

--- example/bundle_evd.py
+++ example/bundle_evd.py
@@ -32,15 +32,16 @@ try:
 
     sleep(5)
 
-    print('=== recharge EVD ===')
-    recharge_evd = api.rechargeEVD(brand_id=24, pin_value=0.5, number='0712359809')
+    print('=== recharge EVD ==='
+    # TODO: replace with proper args
+    recharge_evd = api.rechargeEVD(brand_id, pin_value, number)
     print('Recharge EVD: ')
     pprint.pprint(recharge_evd)
 
     sleep(5)
 
     print('=== recharge bundle ===')
-    recharge_bndl = api.dataBundleRecharge(product_code='SMSD5', number='0778060126')
+    recharge_bndl = api.dataBundleRecharge(product_code, number)
     print('Recharge bundle: ')
     pprint.pprint(recharge_bndl)
 
